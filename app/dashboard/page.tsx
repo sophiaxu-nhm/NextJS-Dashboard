623c2c222d5b61db2d7a06a6e373179f70970108
Sophia X <131772407+sophiaxu-nhm@users.noreply.github.com>
2024-09-05 14:52:31 +0100

    Chapter 11

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -21,9 +21,9 @@ const  {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card title="Collected" value='' type="collected" />
+        <Card title="Pending" value='' type="pending" />
+        <Card title="Total Invoices" value='' type="invoices" />
         <Card
           title="Total Customers"
           value={numberOfCustomers}
