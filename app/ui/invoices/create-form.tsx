9038994ee9fc92a23e12e2437e812bb0e7a5058f
Sophia X <131772407+sophiaxu-nhm@users.noreply.github.com>
2024-09-16 16:24:21 +0100

    Update error message

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -11,7 +11,6 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 import { createInvoice, State } from '@/app/lib/actions';
-// import { useFormState } from 'react-dom';
 
 // @ts-ignore
 import { useActionState } from 'react';
@@ -76,8 +75,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
             <div id="amount-error" aria-live="polite" aria-atomic="true">
-        {state.errors?.customerId &&
-          state.errors.customerId.map((error: string) => (
+        {state.errors?.amount &&
+          state.errors.amount.map((error: string) => (
             <p className="mt-2 text-sm text-red-500" key={error}>
               {error}
             </p>
@@ -125,8 +124,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               </div>
             </div>
             <div id="customer-error" aria-live="polite" aria-atomic="true">
-        {state.errors?.customerId &&
-          state.errors.customerId.map((error: string) => (
+        {state.errors?.status &&
+          state.errors.status.map((error: string) => (
             <p className="mt-2 text-sm text-red-500" key={error}>
               {error}
             </p>
