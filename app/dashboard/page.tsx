e02a2668f82e36f365e4273b4b4f03c732b75cd7
Sophia X <131772407+sophiaxu-nhm@users.noreply.github.com>
2024-09-16 17:06:54 +0100

    Add metadata

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,6 +12,12 @@ import {
 } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Home',
+};
+
 export default async function Page() {
   const revenue = await fetchRevenue();
 const latestInvoices  = await fetchLatestInvoices();
